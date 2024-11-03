e4b60ebb403a845d75f997653778eb81b74d9a75
fabriciofabras <fabriciofabras@gmail.com>
2024-11-03 17:05:07 -0600

    Menu

--- src/components/Cotiza.jsx
+++ src/components/Cotiza.jsx
@@ -12,7 +12,7 @@ export const Cotiza = () => {
             <div class="absolute inset-x-0 -top-40 -z-10 overflow-hidden  sm:-top-80" aria-hidden="true">
                 <div ></div>
             </div>
-            <div class="mx-auto max-w-full py-32 sm:py-48 lg:py-40  lg-w-max">
+            <div class="mx-auto max-w-full py-32 lg-w-max">
                 {/*  <div class="hidden sm:mb-8 sm:flex sm:justify-center">
                     <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                         Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,51 @@
 import { NavLink } from "react-router-dom";
-import { Col, Row } from "react-bootstrap";
 import logo from "../assets/logo.png"
-import { ChatBubbleOvalLeftIcon, HomeIcon } from '@heroicons/react/24/outline'; // Outline style
-import { UserGroupIcon } from '@heroicons/react/24/outline'; // Outline style
+import { HomeIcon } from '@heroicons/react/24/outline'; // Outline style
 import { BuildingOfficeIcon } from '@heroicons/react/24/outline'; // Outline style
-import { FaceSmileIcon } from '@heroicons/react/24/outline'; // Outline style
-
+import { useState } from 'react';
 
 export const NavBar = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+
     return (
-        <nav class="flex items-center justify-between p-1 lg:px-12 px-20 bg-white" aria-label="Global">
+        <nav class="flex items-center justify-around pt-4 lg:px-12 px-4 bg-white border-2" aria-label="Global">
+            <div className="flex lg:hidden">
+                <button
+                    type="button"
+                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                    onClick={toggleMenu}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+            </div>
+
+            <div className={`lg:hidden ${isMenuOpen ? 'block' : 'hidden'} absolute top-16 left-0 right-0 bg-white p-4 shadow-lg`}>
+                <NavLink className="block py-2 text-gray-900 hover:bg-gray-100" to="/inicio">
+                    <HomeIcon className="h-6 w-6 inline-block mr-2" />
+                    Inicio
+                </NavLink>
+                <NavLink className="block py-2 text-gray-900 hover:bg-gray-100" to="/cotiza">
+                    <BuildingOfficeIcon className="h-6 w-6 inline-block mr-2" />
+                    Promociones
+                </NavLink>
+                {/* Añade más enlaces si es necesario */}
+            </div>
             <div class="flex lg:flex-1">
                 <a href="#" class="-m-1.5 p-1.5">
                     <span class="sr-only">Travel Friends</span>
                     <img class="h-20 w-auto" src={logo} alt=""></img>
                 </a>
             </div>
-            <div class="flex lg:hidden">
-                <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                </button>
-            </div>
+            
             <div class="hidden lg:flex lg:gap-x-12">
                 <NavLink className="navlink"
                     to="/inicio"
