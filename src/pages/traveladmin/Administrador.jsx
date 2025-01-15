3ec5f96e7894196b2cbaae3e3a92c8b3df4f190c
fabriciofabras <fabriciofabras@gmail.com>
2025-01-15 11:09:47 -0600

    Consentimiento google administrador

--- src/pages/traveladmin/Administrador.jsx
+++ src/pages/traveladmin/Administrador.jsx
@@ -10,6 +10,18 @@ function Administrador() {
   const [activePage, setActivePage] = useState("Quotes");
 
   const renderPage = () => {
+
+    const clientID = "105019582790-0k24vbs49hq850f60s4cgcerka7o4dih.apps.googleusercontent.com";  // Reemplaza con tu Client ID
+    const redirectUri = "https://travel-friends-mu.vercel.app/administrador"; // Reemplaza con tu URL de redirección
+    const scope = "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email";
+    const responseType = "token"; // O "code" si planeas usarlo en el backend para obtener el token de acceso
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientID}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}&include_granted_scopes=true`;
+
+    // Redirigir a la página de autenticación de Google
+    window.location.href = authUrl;
+
+    // validarUsuario(false);
+
     switch (activePage) {
       case "Quotes":
         return <Quotes />;
