41cf3478d7887c0bc8f80e23863688788e36a3a4
fabriciofabras <fabriciofabras@gmail.com>
2025-01-15 11:38:29 -0600

    Correción redireccionamiento

---
+++ src/pages/traveladmin/Ingreso.jsx
@@ -0,0 +1,24 @@
+
+function Ingreso() {
+
+    const renderPage = () => {
+
+        const clientID = "105019582790-0k24vbs49hq850f60s4cgcerka7o4dih.apps.googleusercontent.com";  // Reemplaza con tu Client ID
+        const redirectUri = "https://travel-friends-mu.vercel.app/administrador"; // Reemplaza con tu URL de redirección
+        const scope = "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email";
+        const responseType = "token"; // O "code" si planeas usarlo en el backend para obtener el token de acceso
+        const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientID}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}&include_granted_scopes=true`;
+
+        // Redirigir a la página de autenticación de Google
+        window.location.href = authUrl;
+    }
+
+    renderPage();
+
+    return (
+        <div className="dashboard mt-24">
+            <div className="content">Ingresar</div>
+        </div>
+    )
+}
+export default Ingreso;
--- src/routes/TravelRoutes.jsx
+++ src/routes/TravelRoutes.jsx
@@ -8,6 +8,7 @@ import { Favoritos } from "../components/Favoritos"
 import { Hotel } from "../components/Hotel"
 import Administrador from "../pages/traveladmin/Administrador"
 import QuoteView from "../pages/traveladmin/QuoteView"
+import Ingreso from "../pages/traveladmin/Ingreso"
 
 export const TravelRoutes = () => {
 
@@ -18,25 +19,26 @@ export const TravelRoutes = () => {
             <div class="bg-white">
                 <header class="absolute inset-x-0 top-0 z-50">
                     <NavBar></NavBar>
-                    
+
                 </header>
             </div>
-            
- 
+
+
 
             <div>
                 <Routes>
-                <Route path="/" element={<Inicio />}></Route>
-                <Route path="/inicio" element={<Inicio />}></Route>
-                <Route  path="/administrador" element={<Administrador/>}></Route>
-                <Route  path="/quote" element={<QuoteView/>}></Route>
-                <Route path="/nosotros" element={<Nosotros />}></Route>
-                <Route path="/cotiza" element={<Cotiza />}></Route>
-                <Route path="/contacto" element={<Contacto />}></Route>
-                <Route path="/favoritos" element={<Favoritos />}></Route>
-                <Route path="/hotel/:destinoId/:hotelId" element={<Hotel />}></Route>
+                    <Route path="/" element={<Inicio />}></Route>
+                    <Route path="/inicio" element={<Inicio />}></Route>
+                    <Route path="/administrador" element={<Administrador />}></Route>
+                    <Route path="/ingreso" element={<Ingreso />}></Route>
+                    <Route path="/quote" element={<QuoteView />}></Route>
+                    <Route path="/nosotros" element={<Nosotros />}></Route>
+                    <Route path="/cotiza" element={<Cotiza />}></Route>
+                    <Route path="/contacto" element={<Contacto />}></Route>
+                    <Route path="/favoritos" element={<Favoritos />}></Route>
+                    <Route path="/hotel/:destinoId/:hotelId" element={<Hotel />}></Route>
                 </Routes>
-            </div> 
+            </div>
 
 
         </>
