d9b438fd421d52ceb8325eac349b0afbe9d3b675
fabriciofabras <fabriciofabras@gmail.com>
2025-07-15 19:35:18 -0600

    politicas y terminos

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
          <footer className="bg-[#004c97] text-white py-4 text-center">
                 <p>&copy; 2025 Travel Friends. Todos los derechos reservados.</p>
                 <div className="flex justify-center mt-4 space-x-4">
-                    <a href="/politicas" className="hover:underline">
+                    <a href="/politica-de-privacidad" className="hover:underline">
                         Políticas de Privacidad
                     </a>
-                    <a href="/terminos" className="hover:underline">
+                    <a href="/terminos-y-condiciones" className="hover:underline">
                         Términos y Condiciones
                     </a>
                 </div>
